74dd6cb14043abfac177e6abcfdbe6dca1a59a60
Артём Мещерин <72318993+st091150@users.noreply.github.com>
2022-11-21 00:53:54 +0300

    Update model.py

--- core/sentiment_analysis_models/rubert_base_cased/model.py
+++ core/sentiment_analysis_models/rubert_base_cased/model.py
@@ -1,15 +1,20 @@
 import torch
-from transformers import AutoModelForSequenceClassification
-from transformers import BertTokenizerFast
+from transformers import BertForSequenceClassification,BertTokenizer
 
-model = AutoModelForSequenceClassification.from_pretrained('core/sentiment_analysis_models/rubert_base_cased/model_data/model')
-tokenizer = BertTokenizerFast.from_pretrained('core/sentiment_analysis_models/rubert_base_cased/model_data/tokenizer')
+tokenizer = BertTokenizer.from_pretrained("DeepPavlov/rubert-base-cased")
+model = BertForSequenceClassification.from_pretrained("DeepPavlov/rubert-base-cased", 
+                                                      num_labels = 3, 
+                                                      output_attentions = False,
+                                                      output_hidden_states = False)
+model.load_state_dict(torch.load(PATH)) #PATH - путь к файлу model_1.pth
+model.eval()
 
 def rubert_base_cased(text):
     # (str) -> int
-    inputs = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
-    outputs = model(**inputs)
-    predicted = torch.nn.functional.softmax(outputs.logits, dim=1)
-    predicted = torch.argmax(predicted, dim=1).numpy()
-    return predicted[0]
+    inputs = tokenizer(text,max_length = 65,padding = 'max_length',truncation = True,return_tensors='pt')
+    with torch.no_grad():
+      model_output = model(**inputs)
+    output = model_output.logits
+    output = torch.nn.functional.softmax(output)
+    return int(output.argmax(axis=1))
 
