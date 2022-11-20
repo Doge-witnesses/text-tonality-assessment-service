d166bc218128cc52ecd50c4a1c1bb483aef00625
Артём Мещерин <72318993+st091150@users.noreply.github.com>
2022-11-21 01:08:56 +0300

    Update model.py

--- core/sentiment_analysis_models/rubert_base_cased/model.py
+++ core/sentiment_analysis_models/rubert_base_cased/model.py
@@ -1,12 +1,13 @@
 import torch
 from transformers import BertForSequenceClassification,BertTokenizer
+map_location=torch.device('cpu')
 
 tokenizer = BertTokenizer.from_pretrained("DeepPavlov/rubert-base-cased")
 model = BertForSequenceClassification.from_pretrained("DeepPavlov/rubert-base-cased", 
                                                       num_labels = 3, 
                                                       output_attentions = False,
                                                       output_hidden_states = False)
-model.load_state_dict(torch.load(PATH)) #PATH - путь к файлу model_1.pth
+model.load_state_dict(torch.load(PATH,map_location=map_location)) #PATH - путь к файлу model_1.pth
 model.eval()
 
 def rubert_base_cased(text):
